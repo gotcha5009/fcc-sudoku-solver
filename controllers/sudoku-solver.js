8c4cb1a8a61d03a6feaafbf1ee392b34bed8efd5
Weerapat Techaudomtaworn <gotcha5009@hotmail.com>
2021-02-15 17:06:49 +0200

    validate function

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,6 +1,27 @@
 class SudokuSolver {
 
   validate(puzzleString) {
+    try {
+      if (puzzleString.length != 81) {
+        return {
+          result: false,
+          error: "Expected puzzle to be 81 characters long"
+        };
+      } else {
+        const regex = /[1-9.]{81}/g;
+        const valid = regex.test(puzzleString);
+        valid
+          ? {
+            result: true
+          }
+          : {
+            result: false,
+            error: "Invalid characters in puzzle"
+          }
+      }
+    } catch (err) {
+      throw err;
+    }
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
@@ -16,7 +37,7 @@ class SudokuSolver {
   }
 
   solve(puzzleString) {
-    
+
   }
 }
 
