289218f521121cf18c488c6dd229df2c055f852a
Weerapat Techaudomtaworn <gotcha5009@hotmail.com>
2021-03-07 13:45:41 +0200

    api check & change nodemon command

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,21 +1,31 @@
 class SudokuSolver {
-
+  constructor() {
+    this.coordination = ['a1', 'a2', 'a3', 'a4', 'a5', 'a6', 'a7', 'a8', 'a9',
+      'b1', 'b2', 'b3', 'b4', 'b5', 'b6', 'b7', 'b8', 'b9',
+      'c1', 'c2', 'c3', 'c4', 'c5', 'c6', 'c7', 'c8', 'c9',
+      'd1', 'd2', 'd3', 'd4', 'd5', 'd6', 'd7', 'd8', 'd9',
+      'e1', 'e2', 'e3', 'e4', 'e5', 'e6', 'e7', 'e8', 'e9',
+      'f1', 'f2', 'f3', 'f4', 'f5', 'f6', 'f7', 'f8', 'f9',
+      'g1', 'g2', 'g3', 'g4', 'g5', 'g6', 'g7', 'g8', 'g9',
+      'h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'h7', 'h8', 'h9',
+      'i1', 'i2', 'i3', 'i4', 'i5', 'i6', 'i7', 'i8', 'i9'];
+  }
   validate(puzzleString) {
     try {
       if (puzzleString.length != 81) {
         return {
-          result: false,
+          valid: false,
           error: "Expected puzzle to be 81 characters long"
         };
       } else {
         const regex = /[1-9.]{81}/g;
         const valid = regex.test(puzzleString);
-        valid
+        return valid
           ? {
-            result: true
+            valid: true
           }
           : {
-            result: false,
+            valid: false,
             error: "Invalid characters in puzzle"
           }
       }
@@ -25,16 +35,66 @@ class SudokuSolver {
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
+    try {
+      let myRow = '';
+      this.coordination.forEach((element, index) => {
+        if (element.includes(row)) {
+          myRow += puzzleString[index];
+        }
+      });
+      return myRow.includes(value) ? false : true;
 
+    } catch (err) {
+      throw err;
+    }
   }
 
   checkColPlacement(puzzleString, row, column, value) {
+    try {
+      let myColumn = '';
+      this.coordination.forEach((element, index) => {
+        if (element.includes(column)) {
+          myColumn += puzzleString[index];
+        }
+      });
+      return myColumn.includes(value) ? false : true;
 
+    } catch (err) {
+      throw err;
+    }
   }
 
   checkRegionPlacement(puzzleString, row, column, value) {
+    //get row region: convert alphabet to their ascii's ref number
+    //then classify it into group of 3 using divide and modulus 
 
+    //get column region: similar to row region, only need divide and modulus since it's already a number
+
+
+    try {
+      function getGroupRef(row, column) {
+        const rowGroupRef = Math.floor((row.charCodeAt(0) - 97) / 3);
+        const colGroupRef = Math.floor((column - 1) / 3);
+        return {
+          row: rowGroupRef,
+          column: colGroupRef
+        }
+      }
+      let myRegion = '';
+      const groupRef = getGroupRef(row, column);
+      this.coordination.forEach((element, index) => {
+        if (JSON.stringify(getGroupRef(element.charAt(0), element.charAt(1))) == JSON.stringify(groupRef)) {
+          myRegion += puzzleString[index];
+        }
+      });
+      //console.log(myRegion);
+      return myRegion.includes(value) ? false : true;
+
+    } catch (err) {
+      throw err;
+    }
   }
+
 
   solve(puzzleString) {
 
--- routes/api.js
+++ routes/api.js
@@ -3,16 +3,69 @@
 const SudokuSolver = require('../controllers/sudoku-solver.js');
 
 module.exports = function (app) {
-  
+
   let solver = new SudokuSolver();
 
   app.route('/api/check')
     .post((req, res) => {
-
+      if (req.body.puzzle == '' || req.body.coordinate == '' || req.body.value == '') {
+        res.json({ "error": "Required field(s) missing" });
+      } else if (!solver.validate(req.body.puzzle).valid) {
+        res.json(valid.error);
+      } else if (!/^[A-Ia-i][1-9]$/.test(req.body.coordinate)) {
+        res.json({ error: 'invalid coordinate' });
+      } else if (!/[1-9]/.test(req.body.value)) {
+        res.json({ error: 'invalid value' });
+      } else {
+        const row = req.body.coordinate.charAt(0);
+        const col = req.body.coordinate.charAt(1);
+        const rowValid = solver.checkRowPlacement(req.body.puzzle, row, col, req.body.value);
+        const colValid = solver.checkColPlacement(req.body.puzzle, row, col, req.body.value);
+        const regionValid = solver.checkRegionPlacement(req.body.puzzle, row, col, req.body.value);
+        const conflict = { valid: false, conflict: [] };
+        if (rowValid && colValid && regionValid) {
+          res.json({ valid: true });
+        } else {
+          if (!solver.checkRowPlacement(req.body.puzzle, row, col, req.body.value)) {
+            conflict.conflict.push('row');
+          }
+          if (!solver.checkColPlacement(req.body.puzzle, row, col, req.body.value)) {
+            conflict.conflict.push('column');
+          }
+          if (!solver.checkRegionPlacement(req.body.puzzle, row, col, req.body.value)) {
+            conflict.conflict.push('region');
+          }
+          res.json(conflict);
+        }
+      }
     });
-    
+
   app.route('/api/solve')
     .post((req, res) => {
 
     });
+
+  app.route('/api/checkValid')
+    .post((req, res) => {
+      const result = solver.validate(req.body.puzzle);
+      res.send(result);
+    });
+
+  app.route('/api/checkRow')
+    .post((req, res) => {
+      const result = solver.checkRowPlacement(req.body.puzzle, req.body.row, req.body.column, req.body.value);
+      res.send(result);
+    });
+
+  app.route('/api/checkCol')
+    .post((req, res) => {
+      const result = solver.checkColPlacement(req.body.puzzle, req.body.row, req.body.column, req.body.value);
+      res.send(result);
+    })
+
+  app.route('/api/checkReg')
+    .post((req, res) => {
+      const result = solver.checkRegionPlacement(req.body.puzzle, req.body.row, req.body.column, req.body.value);
+      res.send(result);
+    })
 };
